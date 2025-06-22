3860ade16d186151992c716dd44736c347a1a4aa
AnthRom <luis.romero@utec.edu.pe>
2025-06-22 18:01:57 -0500

    cambio-realizado

---
+++ api-diagrama/Listar-Diagrama.py
@@ -0,0 +1,32 @@
+import os
+import boto3
+
+BUCKET_NAME = os.environ["BUCKET_NAME"]
+
+def listar_diagramas(event, context):
+    s3 = boto3.client("s3")
+    tipos = {"aws": [], "er": []}
+
+    objetos = s3.list_objects_v2(Bucket=BUCKET_NAME).get("Contents", [])
+
+    for obj in objetos:
+        key = obj["Key"]
+        url = s3.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": BUCKET_NAME, "Key": key},
+            ExpiresIn=3600
+        )
+
+        if key.startswith("aws/"):
+            tipos["aws"].append(url)
+        elif key.startswith("er/"):
+            tipos["er"].append(url)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(tipos),
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+    }
